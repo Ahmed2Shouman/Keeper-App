37d9222b7542ef2251679093f35dcea0298e4f88
Ahmed Shouman <145878832+Ahmed2Shouman@users.noreply.github.com>
2025-06-09 19:29:47 +0300

    added props

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Note from './components/Note'
+import notes from './notes'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +10,14 @@ function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(noteItem => (
+        <Note
+          key={noteItem.id}
+          title={noteItem.title}
+          content={noteItem.content}
+          date={noteItem.date}
+        />
+      ))}
       <Footer />
     </div>
   )
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function Note() {
+export default function Note(props) {
     return (
         <div className="bg-white shadow-md rounded-lg p-5 w-60 m-4 float-left">
-            <h1 className="text-lg mb-1 font-bold">This is the note title</h1>
-            <p className="text-base mb-2 whitespace-pre-wrap break-words font-thin">This is the note content</p>
+            <h1 className="text-lg mb-1 font-bold">{props.title}</h1>
+            <p className="text-base mb-2 whitespace-pre-wrap break-words font-thin">{props.content}</p>
         </div>
     );
 }
